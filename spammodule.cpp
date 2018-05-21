d5cf91d939a5f9ed97dd10ff0c19b5b539185931
Niklas Fiedler <git@nfiedler.de>
2018-05-22 00:09:45 +0200

    added maskImg

--- spammodule.cpp
+++ spammodule.cpp
@@ -72,6 +72,44 @@ static PyObject* trace3D(PyObject *self, PyObject *args) {
     return PyFloat_FromDouble(sum);
 }
 
+static PyObject* maskImg(PyObject *self, PyObject *args) {
+    PyArrayObject *image, *mask;
+     
+
+    if (!PyArg_ParseTuple(args, "O!O!", &PyArray_Type, &image, &PyArray_Type, &mask))
+        return NULL;
+    if (image->nd != 3 || image->descr->type_num != PyArray_UBYTE) {
+        PyErr_SetString(PyExc_ValueError,
+            "image must be three-dimensional and of type numpy.uint8");
+        return NULL;
+    }
+    if (mask->nd != 3 || mask->descr->type_num != PyArray_UBYTE) {
+        PyErr_SetString(PyExc_ValueError,
+            "mask must be three-dimensional and of type numpy.uint8");
+        return NULL;
+    }
+    
+
+    npy_intp maskDims[] = {image->dimensions[0], image->dimensions[1]};
+    int maskDimsNd = 2; // two dimensional output image
+    PyArrayObject* maskedImg = (PyArrayObject*) PyArray_SimpleNew(maskDimsNd, maskDims, PyArray_UBYTE);
+    unsigned char* pixel; 
+    for (int y = 0; y < image->dimensions[0]; y++) {
+        for (int x = 0; x < image->dimensions[1]; x++) {
+            pixel = (unsigned char*) image->data + y * image->strides[0] + x * image->strides[1];
+            if (*(mask->data + pixel[0] * mask->strides[0] + pixel[1] * mask->strides[1] + pixel[2] * mask->strides[2])) {
+                *(maskedImg->data + y * maskedImg->strides[0] + x * maskedImg->strides[1]) = 255;
+            }
+            else {
+                *(maskedImg->data + y * maskedImg->strides[0] + x * maskedImg->strides[1]) = 0;
+            }
+        }
+    }
+
+    //std::raise(SIGINT);
+    return PyArray_Return(maskedImg);
+}
+
 static PyObject* spam_system_plus_1(PyObject* self, PyObject* args) {
     const char* command;
     int sts;
@@ -115,6 +153,7 @@ static PyMethodDef SpamMethods[] = {
     {"trace3D",  trace3D, METH_VARARGS, ""},
     {"getNpArray",  npArray, METH_NOARGS, "returns a simple numpy array."},
     {"getNpArray2D",  npArray2D, METH_NOARGS, "returns a simple 2d numpy array."},
+    {"maskImg", maskImg, METH_VARARGS, ""},
     {NULL, NULL, 0, NULL}
 };
 
